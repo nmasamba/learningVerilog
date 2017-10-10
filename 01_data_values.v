9aae4d884812e3c68a36314702c0d7cefbbc77c2
Nyasha Masamba <nm16667.2016@my.bristol.ac.uk>
2017-10-10 20:17:05 +0100

    Verilog data values

---
+++ 01_data_values.v
@@ -0,0 +1,9 @@
+
+/* Verilog's underlying data representation allows for any bit to have one of four values:
+
+1, 
+0, 
+x (i.e. unknown, could actually be 1, 0, z or on the state of change), 
+z (the high impedance output of a tri-state gate)
+
+*/
